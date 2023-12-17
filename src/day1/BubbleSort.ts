7400dec983058fbfb09036447ae29ad0c3647870
Bazil <bazil.khan@urbaneyes.ai>
2023-12-17 07:19:07 -0500

    completed bubblesort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,15 @@
 export default function bubble_sort(arr: number[]): void {
+    let n = arr.length
 
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+
+            if (arr[j] > arr[j+1]) {
+                // swap
+                let temp = arr[j+1]
+                arr[j+1] = arr[j]
+                arr[j] = temp
+            }
+        }
+    }
 }
