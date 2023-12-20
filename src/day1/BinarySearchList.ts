9d92949a189ad9bd3d161b127922a538f50050a6
Bazil <king.bazil.khan@gmail.com>
2023-12-20 04:43:53 -0500

    completes binary search list

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,20 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
 
+  let low = 0; 
+  let high = haystack.length - 1;
+
+  while (low < high) {
+    const mid = Math.floor((low+high)/2);
+    if (haystack[mid] < needle) {
+      low = mid + 1;
+    }  else if (haystack[mid] >= needle) {
+      high = mid;
+    }
+
+  }
+  if (haystack[low] === needle) {
+    return true;
+  }
+  return false;
+
 }
