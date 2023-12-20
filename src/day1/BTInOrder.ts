6472ca895eb867d4c9b61997c130073ceb131a57
Bazil <king.bazil.khan@gmail.com>
2023-12-20 07:51:01 -0500

    completes BT in order

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,17 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+function helper(node: BinaryNode<number> | null, result: number[]) {
+  if (node === null) {
+    return;
+  }
 
+  helper(node.left, result);
+  result.push(node.value);
+  helper(node.right, result);
+}
+
+export default function in_order_search(head: BinaryNode<number> | null): number[] {
+  let ans: number[] = [];
+  if (head !== null) {
+    helper(head, ans);
+  }
+  return ans;
 }
